97b7e8dcb643f8d806e04d0b079261febab17ddf
Fiona Lapham <fionadlapham@gmail.com>
2018-05-28 09:17:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
     end.first(x)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_student_in_grade_10
     self.all.find_all do |student|
       student.grade == "10"
     end.first
